bd44f40a98133a10f543329de5da404b43b3d8a3
Abishake-5 <ghanabiswa5@gmail.com>
2022-04-19 12:33:55 -0400

    ACTION AND REDUCER FOLDER

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,9 +1,15 @@
+import { string } from "prop-types";
+
 export const SET_MOVIES = "SET_MOVIES";
 export const SET_FILTER = "SET_FILTER";
 
 export function setMovies( value ){
-    return { type: SET_MOVIES , value };
+    return { 
+        type: SET_MOVIES ,
+        value };
 }
 export function setFilter( value ){
-    return { type: SET_FILTER , value };
+    return { 
+        type: SET_FILTER ,
+         value };
 }
